ee7c937e52ba044f397ac913c891ac36b5eba814
rohan <rohanbansode2531@gmail.com>
2026-09-21 19:26:30 +0530

    Initial commit

---
+++ src/test/java/com/project/Course/Platform/CoursePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Course.Platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
